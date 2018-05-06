7d5b129360f895096f44f0c34c1a72bfa4f67417
allen <allen@doodhwala.net>
2018-05-06 16:02:27 +0530

    Cache Model Creation

--- models/cache.js
+++ models/cache.js
@@ -2,6 +2,7 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+
 let CacheSchema = new Schema({
     key : {type: String, required: true},
     value : {type: String, required: true},
