5a84c15a4e2ff383b11e72763abf582c40beb621
allen <allen@doodhwala.net>
2018-05-06 19:49:13 +0530

    Added more comments

--- app/caches/CacheController.js
+++ app/caches/CacheController.js
@@ -11,23 +11,33 @@ let Cache = require('../../models/cache');
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
-//Get Cache Details
+
+/*
+* GET Method for Cache Deatails
+*
+**/
 router.get('/', function(req, res, next){
+
     if(req.query.key){
-        //Get Cache Details by ID
+        //Get Cache Details by Key
         Cache.findOne({key : req.query.key},function(err, cache){
             if(err){
                 return res.status(500).json({data: [], message : err});
             }
-            //console.log(cache);
             if(cache){
+                console.log("Cache hit");
+                //Check if the ttl expires
                 if(cache.ttl <= new Date){
+                    // Update the value with random string and reset ttl
                     updateCache(req, res);
                 }else{
+                    //return cache details
                     return res.status(200).json({data: cache, message: "cache fetched successfully"});
                 }
                 
             }else{
+                // If the Key is not available create new cache 
+                console.log("Cache miss");
                 createCache(req, res);
             }
         });
@@ -37,6 +47,8 @@ router.get('/', function(req, res, next){
             if(err){
                 return res.status(500).json({data: [], message : err});
             }
+
+            //check the cache is empty or not
             if(cache.length > 0 ){
                 return res.status(200).json({data: cache, message: "cache fetched successfully"});
             }else{
@@ -46,19 +58,34 @@ router.get('/', function(req, res, next){
     }
 });
 
-//Post New Cache
+
+/*
+* POST Method for Saving New Cache
+*
+**/
 router.post('/', function(req, res, next){
+    //Call the common function for create new cache
     createCache(req, res);
 });
 
-//Update Cache Details
+/*
+* PUT Method for Updating Cache Deatails
+*
+**/
 router.put('/', function(req, res, next){
+    //Call the common function for update cache
     updateCache(req, res);
 });
 
-//Delete Cache Details
+/*
+* DELETE Method for deleting Cache Deatails
+*
+**/
 router.delete('/', function(req, res, next){
+
+    //Check if there is key available in the request
     if(req.query.key){
+        //Delete the cache if the key exists
         Cache.findOneAndRemove({ key: req.query.key }, function(err) {
             if (!err) {
                 return res.status(200).json({data: [], message: "cache deleted by key successfully"});
@@ -67,6 +94,7 @@ router.delete('/', function(req, res, next){
             }
         });
     }else{
+        //Delete All the cache 
         Cache.remove({}, (err) => { 
             if (!err) {
                 return res.status(200).json({data: [], message: "All cache deleted successfully"});
@@ -78,17 +106,26 @@ router.delete('/', function(req, res, next){
 });
 
 
-//Common function to Insert cache
+
+/*
+* Insert the new cache if Key Doesn't Exists
+*
+**/
 let createCache = function(req, res){
+    
     if(req.method == "GET"){
         req.body.key = req.query.key
     }
+
+    //Check if the key is exists
     Cache.find({key : req.body.key},function(err, cache){
         if(err){
             return res.status(500).json({data: [], message : err});
         }
+
+        //check if the key already exists
         if(cache.length > 0 ){
-            //Update The Key
+            //Update the cache
             updateCache(req, res);
         }else{
             //Check if Cache Limit exceeded
@@ -98,7 +135,6 @@ let createCache = function(req, res){
                     Cache.findOne({})
                         .sort({ ttl: 1})
                         .exec(function(err, cache_details) {
-                        console.log(cache_details);
                         Cache.findOneAndRemove({ key: cache_details.key }, function(err) {
                             if (err) {
                                 return res.status(500).json({data: [], message : err});
@@ -106,8 +142,8 @@ let createCache = function(req, res){
                         });
                     });
                 }
-            
-                //Insert the Key
+                            
+                //If cache string is not available, generation random string
                 if(!req.body.value){
                     req.body.value = randomstring.generate();
                 }
@@ -117,11 +153,12 @@ let createCache = function(req, res){
                     value : req.body.value
                 }
 
+                //Insert New cache
                 Cache.create(new_cache, function(err, cache){
                     if(err){
                         return res.status(500).json({data: [], message : err});
                     }
-                    return res.status(200).json({data : cache, message : "Cache Created Successfully"});
+                    return res.status(201).json({data : cache, message : "Cache Created Successfully"});
                 });
                 
             });
@@ -130,14 +167,21 @@ let createCache = function(req, res){
 }
 
 
-//Common function to update cache by key
+/*
+* Update the existing cache if Key Exists
+*
+**/
 let updateCache = function(req, res){
     if(req.method == "GET"){
         req.body.key = req.query.key
     }
+
+    //Set random string if the value is not available
     if(!req.body.value){
         req.body.value = randomstring.generate();
     }
+
+    //Update the cache with new value and reset the ttl
     Cache.findOneAndUpdate({key: req.body.key}, {$set:{value: req.body.value, ttl: new Date(+new Date() + config.TTLExpireDuration*60*1000) }}, {new: true}, function(err, cache){
         if(err){
             return res.status(500).json({data: [], message : err});
