e8defeea988edcfa2e8d8d942bba871101c12c02
allen <allen@doodhwala.net>
2018-05-06 17:16:35 +0530

    Added code for Get Cached and Generate Random String Package, Handled creation of cache if not there and update if ttl exceeds

--- app/caches/CacheController.js
+++ app/caches/CacheController.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser')
+var randomstring = require("randomstring");
+
 
 //Cache Model Import
 var Cache = require('../../models/cache');
@@ -12,37 +14,40 @@ router.use(bodyParser.json());
 router.get('/', function(req, res, next){
     if(req.query.key){
         //Get Cache Details by ID
-        return res.status(200).json({data: [], message: "cache fetched by Id successfully"});
+        Cache.findOne({key : req.query.key},function(err, cache){
+            if(err){
+                return res.status(500).json({data: [], message : err});
+            }
+            //console.log(cache);
+            if(cache){
+                if(cache.ttl <= new Date){
+                    updateCache(req, res);
+                }else{
+                    return res.status(200).json({data: cache, message: "cache fetched successfully"});
+                }
+                
+            }else{
+                createCache(req, res);
+            }
+        });
     }else{
         //Get All Cache Details
-        return res.status(200).json({data: [], message: "cache fetched successfully"});
+        Cache.find({},function(err, cache){
+            if(err){
+                return res.status(500).json({data: [], message : err});
+            }
+            if(cache.length > 0 ){
+                return res.status(200).json({data: cache, message: "cache fetched successfully"});
+            }else{
+                return res.status(200).json({data: [], message: "cache is empty"});
+            }
+        });
     }
 });
 
 //Post New Cache
 router.post('/', function(req, res, next){
-    Cache.find({key : req.body.key},function(err, cache){
-        if(err){
-            return res.status(500).json({data: [], message : err});
-        }
-        if(cache.length > 0 ){
-            //Update The Key
-            updateCache(req, res);
-        }else{
-            //Insert the Key
-            let new_cache = {
-                key : req.body.key,
-                value : req.body.value
-            }
-
-            Cache.create(new_cache, function(err, cache){
-                if(err){
-                    return res.status(500).json({data: [], message : err});
-                }
-                return res.status(200).json({data : cache, message : "User Created Successfully"});
-            });
-        }
-    });
+    createCache(req, res);
 });
 
 //Update Cache Details
@@ -59,8 +64,47 @@ router.delete('/', function(req, res, next){
     }
 });
 
+let createCache = function(req, res){
+    if(req.method == "GET"){
+        req.body.key = req.query.key
+    }
+    Cache.find({key : req.body.key},function(err, cache){
+        if(err){
+            return res.status(500).json({data: [], message : err});
+        }
+        if(cache.length > 0 ){
+            //Update The Key
+            updateCache(req, res);
+        }else{
+            //Insert the Key
+            if(!req.body.value){
+                req.body.value = randomstring.generate();
+            }
+
+            let new_cache = {
+                key : req.body.key,
+                value : req.body.value
+            }
+
+            Cache.create(new_cache, function(err, cache){
+                if(err){
+                    return res.status(500).json({data: [], message : err});
+                }
+                return res.status(200).json({data : cache, message : "User Created Successfully"});
+            });
+        }
+    });
+}
+
+
 //Common function to update cache by key
 let updateCache = function(req, res){
+    if(req.method == "GET"){
+        req.body.key = req.query.key
+    }
+    if(!req.body.value){
+        req.body.value = randomstring.generate();
+    }
     Cache.findOneAndUpdate({key: req.body.key}, {$set:{value: req.body.value, ttl: new Date(+new Date() + 2*60*1000) }}, {new: true}, function(err, cache){
         if(err){
             return res.status(500).json({data: [], message : err});
